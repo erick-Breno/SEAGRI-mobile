845e430e7a29adaecb0eebc78ffd2943e222cda7
erick-Breno <erickbreno128@gmail.com>
2025-09-28 21:27:25 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   authDomain: "seagri-eda95.firebaseapp.com",
   databaseURL: "https://seagri-eda95-default-rtdb.firebaseio.com",
   projectId: "seagri-eda95",
-  storageBucket: "seagri-eda95.appspot.com",
+  storageBucket: "seagri-eda95.firebasestorage.app",
   messagingSenderId: "53518760667",
   appId: "1:53518760667:web:d234f2988505c705162afe",
   measurementId: "G-HDJ2BM40Y6"
@@ -11,4 +11,5 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.database();
+
 export const storage = firebase.storage();
